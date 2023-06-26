898af36c552317d40273ef8d10fa0b4e6776d411
slowpoke_coding <130159100+slowpokestudycoding@users.noreply.github.com>
2023-06-26 19:07:23 +0800

    parentheses_delete.py

--- parentheses_delete.py
+++ parentheses_delete.py
@@ -1,4 +1,4 @@
 import re
-s = "your str" #enter the sentence in the "your str" which you want to fix ?
+s = "your str" #enter the sentence in the "your str" which you want to fix 
 s = re.sub("\(.*?\)", "", s)
 print(s)
